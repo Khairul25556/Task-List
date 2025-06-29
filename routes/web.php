0a4ee9fcabe57f50c1d428b0562189e64c5b4ff9
Khairul25556 <krft44@gmail.com>
2025-06-29 07:59:16 +0600

    all about routing and implemented some get req. Fixes #1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
 });
+
+Route::get('/xxx', function() {
+    return 'Hello';
+}) ->name('hello'); //to implement route name
+
+//redirect URL
+Route::get('/hallo', function(){
+    return redirect() -> route('hello'); //it is using the route name. So if we change the get hello url , it wont afttect the redirect because it will follow the nanme
+});
+
+Route::get('/greet/{name}', function($name){
+    return 'Hello ' . $name . ' !';
+});
+
+Route::get('/fan/{fanname}', function($fanName){
+    return 'This ' . $fanName . ' is so powerful.';
+});
+
+//if no other route is matched we can use fallback route
+Route::fallback(function() {
+    return 'Still got somewhere!';
+});
+
